121e540f83efe3d7aa91e771841a558beb7369e8
jesussalinas07 <salinas.javier073@gmail.com>
2024-06-27 09:43:33 -0500

    test with vi
    test with vi

--- frontend/assets/scripts/cards.js
+++ frontend/assets/scripts/cards.js
@@ -14,29 +14,32 @@ async function fetchCardData(uid) {
 
     // Get credit card cards
     try {
-        const response = await fetch(`http://localhost:8080/transaction/creditCard/${uid}`);
+        const response = await fetch(`http://localhost:8080/creditCard/userId/${uid}`);
         if (!response.ok) {
             throw new Error('Network response failed: ' + response.statusText);
         }
 
         const creditCardData = await response.json();
+        cards.push(...creditCardData);
     } catch (error) {
         console.error('Fetch Operation Error (Credit Card):', error);
     }
 
     // Get debit card cards
     try {
-        const response = await fetch(`http://localhost:8080/transaction/debitCard/${uid}`);
+        const response = await fetch(`http://localhost:8080/debitCard/userId/${uid}`);
       
         if (!response.ok) {
             throw new Error('Network response failed: ' + response.statusText);
+        
         }
 
         const debitCardData = await response.json();
+        cards.push(...debitCardData);
     } catch (error) {
         console.error('Fetch Operation Error (Debit Card):', error);
     }
-
+    console.log(cards);
     displayCardData(cards);
 }
 
@@ -63,13 +66,13 @@ function displayCardData(card) {
     debitCardHeader.textContent = 'Debit Card ID';
     headerRow.appendChild(debitCardHeader);
 
-    const amountHeader = document.createElement('th');
-    amountHeader.textContent = 'Amount';
-    headerRow.appendChild(amountHeader);
+    const cardLimitHeader = document.createElement('th');
+    cardLimitHeader.textContent = 'Card Limit';
+    headerRow.appendChild(cardLimitHeader);
 
-    const dateHeader = document.createElement('th');
-    dateHeader.textContent = 'Date';
-    headerRow.appendChild(dateHeader);
+    const balanceHeader = document.createElement('th');
+    balanceHeader.textContent = 'Balance';
+    headerRow.appendChild(balanceHeader);
 
     table.appendChild(headerRow);
 
@@ -104,126 +107,6 @@ function displayCardData(card) {
     cardInfoDiv.appendChild(table);
 }
 
-//Backup of original code
-/*
-// Calls the api endpoint
-async function fetchCards() {
-    try {
-        const response = await fetch('http://localhost:8080/user');
-
-        if (!response.ok) {
-            throw new Error('Network response failed: ' + response.statusText);
-        }
-
-        const data = await response.json();
-
-        displayCards(data);
-    } catch (error) {
-        console.error('Fetch Operation Error:', error);
-    }
-}
-function displayCards(user) {
-    const userInfoDiv = document.getElementById('user-info');
-
-    const table = document.createElement('table');
-    table.classList.add('table', 'table-hover', 'table-light');
-
-    const headerRow = document.createElement('tr');
-
-    // Create table headers for the user table
-    const uidHeader = document.createElement('th');
-    uidHeader.textContent = 'UID';
-    headerRow.appendChild(uidHeader);
-
-    const nameHeader = document.createElement('th');
-    nameHeader.textContent = 'Name';
-    headerRow.appendChild(nameHeader);
-
-    const creditScoreHeader = document.createElement('th');
-    creditScoreHeader.textContent = 'Credit Score';
-    headerRow.appendChild(creditScoreHeader);
-
-    table.appendChild(headerRow);
-
-    // Loop through the data and populate the table using the data
-    //
-    user.forEach(user => {
-        // make a nested loop to match
-        creditCard.forEach(creditCard => {
-            if (user.uid === creditCard.uid) {
-                const dataRow = document.createElement('tr');
-
-                const uidData = document.createElement('td');
-                uidData.textContent = user.uid;
-                dataRow.appendChild(uidData);
-            
-                const nameData = document.createElement('td');
-                nameData.textContent = user.name;
-                dataRow.appendChild(nameData);
-
-                const creditScoreData = document.createElement('td');
-                creditScoreData.textContent = user.creditScore;
-                dataRow.appendChild(creditScoreData);
-
-                // redirects to user's card accounts when row is clicked
-                dataRow.addEventListener('click', () => {
-                    window.location.href = `pages/all-cards.html?uid=${user.uid}`;
-                });
-            
-                table.appendChild(dataRow);
-            }
-        })
-        deditCard.forEach(debitCard => {
-            if (user.uid === debitCard.uid) {
-                const dataRow = document.createElement('tr');
-
-                const uidData = document.createElement('td');
-                uidData.textContent = user.uid;
-                dataRow.appendChild(uidData);
-            
-                const nameData = document.createElement('td');
-                nameData.textContent = user.name;
-                dataRow.appendChild(nameData);
-
-                const creditScoreData = document.createElement('td');
-                creditScoreData.textContent = user.creditScore;
-                dataRow.appendChild(creditScoreData);
-
-                // redirects to user's card accounts when row is clicked
-                dataRow.addEventListener('click', () => {
-                    window.location.href = `pages/all-cards.html?uid=${user.uid}`;
-                });
-            
-                table.appendChild(dataRow);
-            }
-        })
-        const dataRow = document.createElement('tr');
-
-        const uidData = document.createElement('td');
-        uidData.textContent = user.uid;
-        dataRow.appendChild(uidData);
-    
-        const nameData = document.createElement('td');
-        nameData.textContent = user.name;
-        dataRow.appendChild(nameData);
-
-        const creditScoreData = document.createElement('td');
-        creditScoreData.textContent = user.creditScore;
-        dataRow.appendChild(creditScoreData);
-
-        // redirects to user's card accounts when row is clicked
-        dataRow.addEventListener('click', () => {
-            window.location.href = `pages/all-cards.html?uid=${user.uid}`;
-        });
-    
-        table.appendChild(dataRow);
-    })
-
-    userInfoDiv.innerHTML = '';
-    userInfoDiv.appendChild(table);
-    
-}
 
 // creates the table when the page loads
 window.onload = fetchCards;
-*/
